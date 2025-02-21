41c2c416ab56f2bbe6d7e23111606c15e380b320
Carlos Lopez <carlos.alopezfigueroa@gmail.com>
2025-02-20 19:52:01 -0600

    add initial login form and logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardModule, InputTextModule, PasswordModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
